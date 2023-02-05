1dc6f78b32540ea8b7c0ae7bd8ce2b034348dcd1
Abdulwahab O. Jimoh <omoniyi.success@gmail.com>
2023-02-05 09:02:20 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        markpro: ['Mark Pro', 'sans-serif'],
+        Manrope: ["Manrope", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
