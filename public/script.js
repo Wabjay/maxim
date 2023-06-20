94f4f04fe19f2d07f777bf17cf41a29815994690
Abdulwahab O. Jimoh <omoniyi.success@gmail.com>
2023-06-20 21:32:40 +0100

    Responsive

--- public/script.js
+++ public/script.js
@@ -10,11 +10,12 @@ var bottom = ["endless possibilities for","borderless credit for"];
 var i = 1;
 setInterval(function(){
  $(".addLine").toggleClass('line')
- $("#borderless").animate({opacity: 0.2}).animate({opacity: 1}).text(bottom[i])
- $("#endless").animate({opacity: 0.2}).animate({opacity: 1}).text(tops[i])
+//  $("#borderless").animate({opacity: 0.2}).animate({opacity: 1}).text(bottom[i])
+//  $("#endless").animate({opacity: 0.2}).animate({opacity: 1}).text(tops[i])
+ $("#endless").animate({opacity: 0.2}, 300).animate({opacity: 0.5}, 700).animate({opacity: 1}, 1000).text(tops[i])
+ $("#borderless").animate({opacity: 0.2}, 300).animate({opacity: 0.5}, 700).animate({opacity: 1}, 1000 ).text(bottom[i])
  i = (i+1) % bottom.length;
 },4000)
-
 
 
 var countries = ['britain', 'nig', 'usa', 'ghn', 'cnd', 'sa']
