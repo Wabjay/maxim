70c4a47168ed6d52d8e29a96744c26664905f950
Abdulwahab O. Jimoh <omoniyi.success@gmail.com>
2023-06-22 20:13:33 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,18 +13,29 @@ module.exports = {
       screens: {
         'xxl': {'min': '1440px'},
         "ms": {'min': '576px', 'max': '1023px'},
-        "xsm": {'max': '566px'}
+        "xsm": {'max': '566px'},
+        "xxsm": {'max': '320px'}
+
       },
       animation: {
         'animate-bounce': 'bounce 3s infinite',
+        'button-bounce': 'bounce 5s infinite',
       },
       backgroundImage: {
-        'footer-back': "url('./images/footer_bg.svg')",
+        'footer-back': "url('./images/Maxim-mark.svg')",
       },
       backgroundPosition: {
-        "footer-position": 'bottom -28rem left 25rem',
-        "footer-bigger": "bottom -25rem left 33rem"
+        "footer-position": 'bottom -35rem left -10px',
+        "footer-bigger": "bottom -34rem left 27rem",
+        "footer-xtra": "bottom -38rem left 7rem",
       },
+      keyframes:{
+        'slow-bounce': {
+          '0%, 100%':{transform: 'translateY(-25%) animation-timing-function: cubic-bezier(0.8,0,1,1)'},
+          '50%':{transform: 'translateY(0) animation-timing-function: cubic-bezier(0.8,0,1,1)'}
+
+        }
+      }
     },
   },
   plugins: [require('daisyui')],
