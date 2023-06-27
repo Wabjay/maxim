c1342988d7f1d9764140b27a4d3c14e84f40c62b
Abdulwahab O. Jimoh <omoniyi.success@gmail.com>
2023-06-27 04:50:22 +0100

    Waitlist form

--- public/script.js
+++ public/script.js
@@ -1,6 +1,5 @@
 
 
-
 //   Change text 
 
 var tops = ["Endless Possibilities", "Unrestricted Access"];
@@ -24,3 +23,61 @@ setInterval(function(){
 },1000)
 
 $("#date").text(Date().getFullYear())
+
+
+//   function waitlist(){
+//   var thisForm = document.getElementById("waitlist");
+ 
+//   thisForm.addEventListener("submit", function(event) {
+//      event.preventDefault();
+//   // POST the data
+//  const email_address = document.getElementById("email").value
+
+//     console.log(email_address)
+  
+//   });
+//   }
+  
+  
+document
+.getElementById("submit")
+.addEventListener("click", function (event) {
+    event.preventDefault();
+    console.log(event)
+});
+
+function waitlist(){
+
+    const waitlist_pop = document.getElementById("waitlist_popup");
+  var thisForm = document.getElementById("waitlist_form");
+  const headers_ = {
+    // API KEY IN Bearer
+       'Authorization': 'Bearer keyAs1krG80fTzo0c',
+       'Content-Type': 'application/json'
+  };
+  // When the form is submitted...
+  thisForm.addEventListener("submit", function(event) {
+     event.preventDefault();
+  // POST the data
+  axios.post('https://api.airtable.com/v0/appJWS6wauaLZ',
+  {
+     "fields": {
+      "Email": document.getElementById("email").value,
+  }
+  }, {headers: headers_}
+  )
+  .then((resp) => {
+    console.log("success!")
+    waitlist_pop.classList.add("show")
+    if(waitlist_pop.classList.contains("show")) // Check if the popup is shown
+  setTimeout(() => waitlist_pop.classList.remove("show"), 5500);
+  
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  return false
+//   event.target.reset();
+  });
+  }
+waitlist()
