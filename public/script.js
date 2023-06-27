0db0554b5c2330580666707c61e13503a917e9e2
Abdulwahab O. Jimoh <omoniyi.success@gmail.com>
2023-06-27 13:09:08 +0100

    Waitlist

--- public/script.js
+++ public/script.js
@@ -25,59 +25,47 @@ setInterval(function(){
 $("#date").text(Date().getFullYear())
 
 
-//   function waitlist(){
-//   var thisForm = document.getElementById("waitlist");
- 
-//   thisForm.addEventListener("submit", function(event) {
-//      event.preventDefault();
-//   // POST the data
-//  const email_address = document.getElementById("email").value
-
-//     console.log(email_address)
-  
-//   });
-//   }
-  
-  
-document
-.getElementById("submit")
-.addEventListener("click", function (event) {
-    event.preventDefault();
-    console.log(event)
-});
 
 function waitlist(){
+ 
+  const waitlist_pop = document.getElementById("waitlist_popup");
+  const waitlist_text  = document.getElementById("waitlist_text");
+document.getElementById("submit").addEventListener("click", function (event) {
+  event.preventDefault();
 
-    const waitlist_pop = document.getElementById("waitlist_popup");
-  var thisForm = document.getElementById("waitlist_form");
   const headers_ = {
     // API KEY IN Bearer
-       'Authorization': 'Bearer keyAs1krG80fTzo0c',
-       'Content-Type': 'application/json'
-  };
-  // When the form is submitted...
-  thisForm.addEventListener("submit", function(event) {
-     event.preventDefault();
+    'Authorization': 'Bearer keyOpje8r4lQVibqB',
+    'Content-Type': 'application/json'
+};
+const Email = document.getElementById("email").value
+if(Email){
   // POST the data
-  axios.post('https://api.airtable.com/v0/appJWS6wauaLZ',
-  {
-     "fields": {
-      "Email": document.getElementById("email").value,
-  }
-  }, {headers: headers_}
+  axios.post('https://api.airtable.com/v0/apprrJKWwFeEItsWb/waitlist',
+ {
+    "fields": {
+      "Email": document.getElementById("email").value
+    }
+  },
+ {headers: headers_}
   )
   .then((resp) => {
     console.log("success!")
+  document.getElementById("email").value = " "
     waitlist_pop.classList.add("show")
+    waitlist_text.textContent = "Your details has been added to our waitlist."
     if(waitlist_pop.classList.contains("show")) // Check if the popup is shown
-  setTimeout(() => waitlist_pop.classList.remove("show"), 5500);
-  
+  setTimeout(() => waitlist_pop.classList.remove("show"), 3500);
   })
-  .catch(function (error) {
-    console.log(error);
-  })
-  return false
-//   event.target.reset();
-  });
-  }
-waitlist()
+      .catch(function (error) {
+          console.log(error);
+      })}
+      else{
+        waitlist_pop.classList.add("show")
+    waitlist_text.textContent = "Please enter your email address"
+        if(waitlist_pop.classList.contains("show")) // Check if the popup is shown
+      setTimeout(() => waitlist_pop.classList.remove("show"), 3500);
+      }
+}
+)
+}
