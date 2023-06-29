ffc004d931cb616dbb29e63ba5996b5f5db6d8a9
Abdulwahab O. Jimoh <omoniyi.success@gmail.com>
2023-06-29 06:45:07 +0100

    Maxim

--- public/script.js
+++ public/script.js
@@ -1,81 +1,83 @@
 
 
 //   Change text 
-
 var tops = ["Endless Possibilities", "Unrestricted Access"];
 
-var bottom = ["endless possibilities for","borderless credit for"];
+var bottom = ["endless possibilities for", "borderless credit for"];
 
 var i = 1;
-setInterval(function(){
- $(".addLine").toggleClass('line')
- $("#borderless").fadeTo(10 , 0.1).fadeTo(200 , 0.2).fadeTo(200 , 0.3).fadeTo(300 , 0.5).fadeTo(500 , 1).text(bottom[i])
- $("#endless").fadeTo(10 , 0.1).fadeTo(200 , 0.2).fadeTo(200 , 0.3).fadeTo(300 , 0.5).fadeTo(500 , 1).text(tops[i])
- i = (i+1) % bottom.length;
-},4000)
+setInterval(function () {
+  $(".addLine").toggleClass('line');
+  $("#borderless").fadeTo(10, 0.1).fadeTo(200, 0.2).fadeTo(200, 0.3).fadeTo(300, 0.5).fadeTo(500, 1).text(bottom[i]);
+  $("#endless").fadeTo(10, 0.1).fadeTo(200, 0.2).fadeTo(200, 0.3).fadeTo(300, 0.5).fadeTo(500, 1).text(tops[i]);
+  i = (i + 1) % bottom.length;
+}, 4000)
 
 
+// Country display function
 var countries = ['britain', 'nig', 'usa', 'ghn', 'cnd', 'sa']
 var count = 1;
-setInterval(function(){
- $("#country").attr('src', `./public/images/${countries[count]}.png`).fadeIn(100)
- count= (count+1) % countries.length;
-},1000)
+setInterval(function () {
+  $("#country").attr('src', `./public/images/${countries[count]}.png`).fadeIn(100)
+  count = (count + 1) % countries.length;
+}, 1000);
 
-$("#date").text(Date().getFullYear())
+let date = new Date().getFullYear();
+$("#date").text(date);
 
 
 
-function waitlist(){
-
-const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
- 
+// Waitlist function
+function waitlist() {
+  const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
   const waitlist_pop = document.getElementById("waitlist_popup");
-  const waitlist_text  = document.getElementById("waitlist_text");
-document.getElementById("submit").addEventListener("click", function (event) {
-  event.preventDefault();
+  const waitlist_text = document.getElementById("waitlist_text");
 
-  const headers_ = {
-    // API KEY IN Bearer
-    'Authorization': 'Bearer keyOpje8r4lQVibqB',
-    'Content-Type': 'application/json'
-};
-const email = document.getElementById("email").value
-const Email = emailRegex.test(email)
+  document.getElementById("submit").addEventListener("click", function (event) {
+    event.preventDefault();
 
-if(!Email){
+    const headers_ = {
+      // API KEY IN Bearer
+      'Authorization': 'Bearer keyOpje8r4lQVibqB',
+      'Content-Type': 'application/json'
+    };
+    const email = document.getElementById("email").value
+    const Email = emailRegex.test(email)
 
-  waitlist_pop.classList.add("show")
-    waitlist_text.textContent = "Please enter a correct email address"
-    if(waitlist_pop.classList.contains("show")) // Check if the popup is shown
-  setTimeout(() => waitlist_pop.classList.remove("show"), 2500);
-}else{
-  // POST the data
-  axios.post('https://api.airtable.com/v0/apprrJKWwFeEItsWb/waitlist',
- {
-    "fields": {
-      "Email": document.getElementById("email").value
-    }
-  },
- {headers: headers_}
-  )
-  .then((resp) => {
-    console.log("success!")
-  document.getElementById("email").value = " "
-    waitlist_pop.classList.add("show")
-    waitlist_text.textContent = "Your details has been added to our waitlist."
-    if(waitlist_pop.classList.contains("show")) // Check if the popup is shown
-  setTimeout(() => waitlist_pop.classList.remove("show"), 3500);
-  })
-      .catch(function (error) {
+    if (!Email) {
+
+      waitlist_pop.classList.add("show")
+      waitlist_text.textContent = "Please enter a correct email address"
+      if (waitlist_pop.classList.contains("show")) // Check if the popup is shown
+        setTimeout(() => waitlist_pop.classList.remove("show"), 2500);
+    } else {
+      // POST the data
+      axios.post('https://api.airtable.com/v0/apprrJKWwFeEItsWb/waitlist',
+        {
+          "fields": {
+            "Email": document.getElementById("email").value
+          }
+        },
+        { headers: headers_ }
+      )
+        .then((resp) => {
+          console.log("success!")
+          document.getElementById("email").value = " "
+          waitlist_pop.classList.add("show")
+          waitlist_text.textContent = "Your details has been added to our waitlist."
+          if (waitlist_pop.classList.contains("show")) // Check if the popup is shown
+            setTimeout(() => waitlist_pop.classList.remove("show"), 3500);
+        })
+        .catch(function (error) {
           console.log(error);
-      })}
-      if(!email){
-        waitlist_pop.classList.add("show")
-    waitlist_text.textContent = "Please enter your email address"
-        if(waitlist_pop.classList.contains("show")) // Check if the popup is shown
-      setTimeout(() => waitlist_pop.classList.remove("show"), 3500);
-      }
-}
-)
+        })
+    }
+    if (!email) {
+      waitlist_pop.classList.add("show")
+      waitlist_text.textContent = "Please enter your email address"
+      if (waitlist_pop.classList.contains("show")) // Check if the popup is shown
+        setTimeout(() => waitlist_pop.classList.remove("show"), 3500);
+    }
+  }
+  )
 }
